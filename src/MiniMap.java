5c7cf261e9eb2e934c58651af1d7fd30d7ea4baa
P-E <p.e.arrouy@gmail.com>
2013-04-15 21:01:03 +0700

    changed some coordinates on the minimap

--- src/MiniMap.java
+++ src/MiniMap.java
@@ -26,14 +26,14 @@ JFrame frame;
 	}
     public void paintMiniMap(Graphics g, int x, int y) {
     		g.setColor(Color.BLACK);
-    		g.fillRect(10,40,220,220);
+    		g.fillRect(10,40,240,240);
     		g.setColor(new Color (185,122,87));
-    		g.fillRect(15,45,210,210);
+    		g.fillRect(15,45,230,230);
     		g.setColor(new Color(0,162,232));
-        	g.fillRect(20,50,200,200);
+        	g.fillRect(20,50,220,220);
         	
         	 
-        	g.drawImage(icone,2*x+20-9,2*y+50-9,frame);
+        	g.drawImage(icone,2*x+30-9,2*y+60-9,frame);
         	
                 
             
